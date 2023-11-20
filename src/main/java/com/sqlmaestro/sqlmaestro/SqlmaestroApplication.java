147c8bf52755075c9153cfc1f417a121c1b54763
varun-kajarekar <kajrekarvarun@gmail.com>
2023-11-20 14:45:09 +0530

    Project setup

---
+++ src/main/java/com/sqlmaestro/sqlmaestro/SqlmaestroApplication.java
@@ -0,0 +1,13 @@
+package com.sqlmaestro.sqlmaestro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlmaestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlmaestroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqlmaestro/sqlmaestro/SqlmaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlmaestro.sqlmaestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlmaestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
